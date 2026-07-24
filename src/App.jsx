60a020679954e72b6ddbeb442adcef8d7be62a5a
paulgks <paulgks@paulgksui-iMac.local>
2026-07-24 15:25:06 +0900

    추가 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,81 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [formData, setFormData] = useState({
+    name : '',
+    country : '',
+    city : '',
+    date : '',
+  })
+
+  const initialData = [
+    {
+      id: 1,
+      name: '에펠탑',
+      country: '프랑스',
+      city: '파리',
+      date: '2024-07-15',
+      image: 'https://i.namu.wiki/i/yF485NWCazWq7Zd52f5DzchphPkZ4VGVZXkx57AHIygI_GHgHLQnAa2zVJXZ_hxnid6NA09bKhxSc2FeLguNzw.webp',
+      rating: 5,
+      memo: '정말 아름다웠어요! 야경이 최고였습니다.',
+      createdAt: '2024-07-20T10:30:00'
+    }
+  ]
+
+  const [countries, setCountries] = useState(() => {
+    const saved = localStorage.getItem("countries");
+
+    return saved? JSON.parse(saved) : initialData;
+  })
+
+  const handleAdd = (newCountry) => {
+      setCountries([...countries , {
+        id: countries[countries.length-1].id + 1,
+        ...newCountry
+      } ])
+    }
+  
+
+  const handleDelete = (id) => {
+    setCountries(
+      countries.filter(country => country.id !== id)
+
+    )
+  }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({
+      ...formData,
+      [name] : value
+    })
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAdd(formData)
+  }
+
+  useEffect(() => {
+    console.log(countries)
+  }, [countries])
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+        <input type="text" name="country" value={formData.country} onChange={handleChange}/>
+        <input type="text" name="city" value={formData.city} onChange={handleChange}/>
+        <input type="date" name="date" value={formData.date} onChange={handleChange}/>
+        <button type='submit'>저장</button>
+      </form>
+    </div>
   )
 }
 
