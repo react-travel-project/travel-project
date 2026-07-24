a4e5d83a1132e790fa8de89f9a7298efe5e342dc
SeongmoAhn <asm0619@naver.com>
2026-07-24 16:30:32 +0900

    feat: 최종 merge

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -3,16 +3,24 @@ import './TravelList.css'
 function TravelCard({ travel, onDelete, onEdit }) {
     return (
         <div className="travel-card">
-            <img className="card-image" src="{travels.image}" alt="여행지 사진" />
+            <div className="card-image">
+                {travel.image ? (
+                    <img src={travel.image} alt="여행지 사진" />
+                ) : (
+                    <div className="no-image">✈️</div>
+                )}
+            </div>
             <div className='card-content'>
                 <h3>{travel.name}</h3>
                 <div className="location">{travel.city}, {travel.country}</div>
+                <div className="date">{travel.date}</div>
+                <div className="rating">{'⭐'.repeat(travel.rating)}</div>
+                <div className="memo">{travel.memo}</div>
             </div>
-            <div className="date">{travel.date}</div>
-            <div className="rating">{travel.rating}</div>
-            <div className="memo">{travel.memo}</div>
-            <button className="btn-edit" onClick={onEdit}>수정</button>
-            <button className="btn-delete" onClick={onDelete}>삭제</button>
+            <div className="card-actions">
+                <button className="btn-edit" onClick={() => onEdit(travel)}>수정</button>
+                <button className="btn-delete" onClick={() => onDelete(travel.id)}>삭제</button>
+            </div>
         </div>
     )
 }
